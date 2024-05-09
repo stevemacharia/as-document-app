c8043f3128e58d7c6aa53cbb0e6dc42f6d872fa6
stevemacharia <stevemachariah@outlook.com>
2024-05-09 22:48:49 +0300

    INVOICE MODEL START

--- documents/models.py
+++ documents/models.py
@@ -35,3 +35,22 @@ class QuotationItems(models.Model):
     item_description = models.CharField(max_length=800)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=15, decimal_places=2)
+
+
+class Invoice(models.Model):
+    invoice_id = models.CharField(blank=True, max_length=100)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+    status = models.BooleanField(default="False", null=True, blank=True)
+    quotation_doc = models.FileField(upload_to='quotation_docs', default='default.pdf', null=True, blank=True)
+    submission_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+
+class InvoiceItems(models.Model):
+    invoice = models.ForeignKey(Quotation, on_delete=models.CASCADE)
+    item = models.CharField(max_length=300)
+    item_description = models.CharField(max_length=800)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
