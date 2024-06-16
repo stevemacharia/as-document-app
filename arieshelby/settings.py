53dda1442dbc30f5f2bdc5538055f7ab134e39c9
stevemacharia <stevemachariah@outlook.com>
2024-06-16 13:39:46 -0400

    EMAIL CONFIGS

--- arieshelby/settings.py
+++ arieshelby/settings.py
@@ -228,3 +228,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 USE_THOUSAND_SEPARATOR = True
 
 
+# email config
+DEFAULT_FROM_EMAIL = 'info@arieshelby.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.arieshelby.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'info@arieshelby.com'
+EMAIL_HOST_PASSWORD = 'Q%CCxu-eM7Bp'
