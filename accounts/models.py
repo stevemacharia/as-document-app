c45b3aa5e44db2f83e1f1cd221d909b8b52be2cb
steve macharia <40029452+stevemacharia@users.noreply.github.com>
2024-09-09 18:47:33 +0300

    BUSINESS ACCOUNT MODEL LOGO UPLOAD WORKING

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from PIL import Image
 import os
 
 # Create your models here.
@@ -24,7 +25,7 @@ class BusinessAccount(models.Model):
     phone_number = models.CharField(max_length=100, blank=True, null=True)
     tel = models.CharField(max_length=100, blank=True, null=True)
     theme_color = models.CharField(max_length=100, blank=True, null=True)
-    logo = models.ImageField(upload_to="business_logos/", validators=[validate_image_size], blank=True, null=True)
+    logo = models.ImageField(upload_to="business_logos/",  blank=True, null=True, default='business_logos/AS_LOGO.png')
     # payment_option = models.ForeignKey(PaymentOption, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -32,3 +33,13 @@ class BusinessAccount(models.Model):
 
     class Meta:
         verbose_name = "Business Accounts"
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.logo.path)
+
+        if img.height > 600 or img.width > 600:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save(self.logo.path)
