b197dc3fa7087802b2de8a567f748a06289d7b2e
stevemacharia <stevemachariah@outlook.com>
2024-10-21 21:09:36 +0300

    FINISHED EEDITING, DELETING AND ADDING  OF CLIENT PROFILES

--- accounts/views.py
+++ accounts/views.py
@@ -44,7 +44,7 @@ def payment_account_register(request):
             
             # Redirect to the referrer or a fallback URL if the referrer is not available
             if previous_url:
-                return redirect(previous_url)
+                return redirect('client-address-creation')
             else:
                 # Fallback URL in case referrer is not present
                 return redirect('index') 
@@ -169,6 +169,73 @@ def payment_account_delete(request, id):
         return redirect('index') 
 
 
+@login_required
+def client_edit(request, id):
+    selected_client_profile = Client.objects.get(id=id)
+    # Capture the referrer (previous page URL)
+    previous_url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        client_profile_form_instance = ClientForm(request.POST,  instance=selected_client_profile)
+
+        if client_profile_form_instance.is_valid():
+            client_profile_form_instance.save()
+
+            messages.success(request, f'Client address has been updated!')
+            # Redirect to the referrer or a fallback URL if the referrer is not available
+            if previous_url:
+                return redirect('business-profile')
+            else:
+                # Fallback URL in case referrer is not present
+                return redirect('index') 
+
+    else:
+        client_profile_id = id
+        client_profile_form= ClientForm(instance=selected_client_profile)
+    return render(request, 'accounts/client_edit.html', {'client_profile_form':client_profile_form, 'client_profile_id' : client_profile_id })
+
+
+@login_required
+def client_profile_add(request):
+    # Capture the referrer (previous page URL)
+    previous_url = request.META.get('HTTP_REFERER')
+    business_account_id = request.session.get('selected_business_account')
+    selected_business_account = BusinessAccount.objects.get(id=business_account_id)
+
+    if request.method == 'POST':
+        client_profile_form_instance = ClientForm(request.POST)
+        if client_profile_form_instance.is_valid():
+
+            client_profile_form= client_profile_form_instance.save(commit=False)
+
+            client_profile_form.business_account=selected_business_account
+            client_profile_form.save()
+
+            messages.success(request, f'Client account has been added!')
+            # Redirect to the referrer or a fallback URL if the referrer is not available
+            if previous_url:
+                return redirect('business-profile')
+            else:
+                # Fallback URL in case referrer is not present
+                return redirect('index') 
+
+    else:
+        client_address_form= ClientForm()
+    return render(request, 'accounts/client_profile_add.html', {'client_address_form':client_address_form})
+
+
+@login_required
+def client_profile_delete(request, id):
+    selected_client_profile_option = Client.objects.get(id=id)
+    selected_client_profile_option.delete()
+    # Capture the referrer (previous page URL)
+    previous_url = request.META.get('HTTP_REFERER')
+    messages.success(request, f'Client profile deleted successfully')
+        # Redirect to the referrer or a fallback URL if the referrer is not available
+    if previous_url:
+        return redirect(previous_url)
+    else:
+        # Fallback URL in case referrer is not present
+        return redirect('index') 
 
 # end of busines profile views
 
